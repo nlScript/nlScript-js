4ff548a39286d95c5c7977482b36a14289c07ab0
Benjamin Schmid <bene.schmid@gmail.com>
2023-11-09 22:48:04 +0100

    No highlighting of matching words.
    Don't use codemirror's basic setup, but specify extensions explicitly.

--- src/ui/ACEditor.ts
+++ src/ui/ACEditor.ts
@@ -1,4 +1,4 @@
-import { EditorView, basicSetup } from "codemirror";
+import { EditorView } from "codemirror";
 import { highlight_extension, ParameterizedCompletion } from "./ParameterizedCompletion";
 import { error_highlight_extension, ErrorHighlight } from "./ErrorHighlight";
 import { StateEffect } from "@codemirror/state";
@@ -8,6 +8,48 @@ import { Autocompletion } from "../core/Autocompletion";
 import { ACCompleter } from "./ACCompleter";
 import { ParseException } from "../ParseException";
 import { Matcher } from "../core/Matcher";
+
+
+import {keymap, highlightSpecialChars, drawSelection, highlightActiveLine, dropCursor,
+    rectangularSelection, crosshairCursor,
+    lineNumbers, highlightActiveLineGutter} from "@codemirror/view"
+import {Extension, EditorState} from "@codemirror/state"
+import {defaultHighlightStyle, syntaxHighlighting, indentOnInput, bracketMatching,
+    foldGutter, foldKeymap} from "@codemirror/language"
+import {defaultKeymap, history, historyKeymap} from "@codemirror/commands"
+import {searchKeymap} from "@codemirror/search"
+import {autocompletion, completionKeymap, closeBrackets, closeBracketsKeymap} from "@codemirror/autocomplete"
+import {lintKeymap} from "@codemirror/lint"
+
+const basicSetup: Extension = (() => [
+    lineNumbers(),
+    highlightActiveLineGutter(),
+    highlightSpecialChars(),
+    history(),
+    foldGutter(),
+    drawSelection(),
+    dropCursor(),
+    EditorState.allowMultipleSelections.of(true),
+    indentOnInput(),
+    syntaxHighlighting(defaultHighlightStyle, {fallback: true}),
+    bracketMatching(),
+    closeBrackets(),
+    autocompletion(),
+    rectangularSelection(),
+    crosshairCursor(),
+    highlightActiveLine(),
+    // highlightSelectionMatches(),
+    keymap.of([
+      ...closeBracketsKeymap,
+      ...defaultKeymap,
+      ...searchKeymap,
+      ...historyKeymap,
+      ...foldKeymap,
+      ...completionKeymap,
+      ...lintKeymap
+    ])
+  ])()
+
 
 export class ACEditor {
 
