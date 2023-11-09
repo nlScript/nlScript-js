nlScript/nlScript-js

commit fb63165dc82f59fc29ee9af45c255f8ada6b8265

Avoid that the autocompleter is called several times for one autocompletion.
This would be the case if two end-of-input nodes have the same
autocompleting parent.

To solve this, first collect all the auto-completing parents. These are
all different ParsedNodes, to identify equality, compare their
production, and use only those for autocompletion which have different
productions.

Sync with 110af4a6cc9136e1f3fe0d76e7127b1549560299

src/core/RDParser.ts
@@ -66,18 +66,34 @@ class RDParser {
     }
 
     private collectAutocompletions(endOfInput: SymbolSequence[], autocompletions: Autocompletion[]): void {
-        for(const seq of endOfInput) {
-            this.addAutocompletions(seq, autocompletions);
+        if(autocompletions === null || autocompletions === undefined)
+            throw new Error("autocompletions should be defined at this point");
+
+        const autocompletingParents: DefaultParsedNode[] = [];
+        for (const seq of endOfInput) {
+            this.collectAutocompletingParents(seq, autocompletingParents);
+        }
+
+        const done: Set<string> = new Set();
+        for(const autocompletingParent of autocompletingParents) {
+            const prod = autocompletingParent.getProduction();
+            let key: string;
+            if(prod !== undefined) {
+                key = prod.getLeft().getSymbol() + ":";
+                for(const s of prod.getRight())
+                    key += s.getSymbol();
+            }
+            else {
+                key = autocompletingParent.getSymbol().getSymbol();
+            }
+            if(!done.has(key)) {
+                this.addAutocompletions(autocompletingParent, autocompletions);
+                done.add(key);
+            }
         }
     }
 
-    private addAutocompletions(symbolSequence: SymbolSequence, autocompletions: Array<Autocompletion | undefined>): void {
-        if(autocompletions === null || autocompletions === undefined)
-            throw new Error("autocompletions should be defined at this point");
-        
-        if(autocompletions.length > 0 && autocompletions.at(-1) === undefined)
-            return;
-        
+    private collectAutocompletingParents(symbolSequence: SymbolSequence, autocompletingParents: Array<DefaultParsedNode>): void {
         let last: DefaultParsedNode[] = [];
         this.createParsedTree(symbolSequence, last);
 
@@ -98,8 +114,13 @@ class RDParser {
                 break;
             }
         }
-        if(autocompletingParent === undefined)
-            return;
+        if(autocompletingParent !== undefined)
+            autocompletingParents.push(autocompletingParent);
+    }
+
+    private addAutocompletions(autocompletingParent: DefaultParsedNode, autocompletions: Array<Autocompletion | undefined>): void {
+        if(autocompletions.length > 0 || autocompletions.at(-1) === undefined)
+            throw new Error("autocompletions should be defined at this point");
         
         let autocompletingParentStart: number = autocompletingParent.getMatcher().pos;
         let alreadyEntered: string = this.lexer.substring(autocompletingParentStart);

commit 772be7e4a3e042ede6bd3e92092253450eba803f

Fix a bug introduced by a typo in fb63165dc82f5

src/core/RDParser.ts
@@ -119,8 +119,8 @@ class RDParser {
     }
 
     private addAutocompletions(autocompletingParent: DefaultParsedNode, autocompletions: Array<Autocompletion | undefined>): void {
-        if(autocompletions.length > 0 || autocompletions.at(-1) === undefined)
-            throw new Error("autocompletions should be defined at this point");
+        if(autocompletions.length > 0 && autocompletions.at(-1) === undefined)
+            return;
         
         let autocompletingParentStart: number = autocompletingParent.getMatcher().pos;
         let alreadyEntered: string = this.lexer.substring(autocompletingParentStart);