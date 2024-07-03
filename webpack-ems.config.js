f0bb8c0f7c5851f762290b99943d0a448eb3cb70
Benjamin Schmid <bene.schmid@gmail.com>
2024-07-03 21:35:42 +0200

    Some project restructuring.
    In principal, follow the guide at
    https://github.com/bene51/typescript-webpack-library-setup/

---
+++ webpack-ems.config.js
@@ -0,0 +1,34 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: 'production',
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    library: {
+      type: 'module'
+    },
+    globalObject: 'this',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  experiments: {
+    outputModule: true,
+  },
+};
+
